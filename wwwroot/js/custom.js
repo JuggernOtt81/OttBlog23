6a1c80716ba33f3bfc281b04b14984a0b07244b1
JuggernOtt81 <juggernott81@gmail.com>
2023-01-31 10:35:18 -0500

    cleaned up the javascript file

--- wwwroot/js/custom.js
+++ wwwroot/js/custom.js
@@ -1,120 +1,37 @@
-﻿//let index = 0;
-
-//function AddTag() {
-//    //use search to detact duplicate tags
-//    var tagEntry = document.getElementById("TagEntry");
-//    let searchResult = null;
-//    searchResult = Search(tagEntry.value);
-
-//    if (searchResult != null) {
-//        swalWithDarkButton.fire({
-//            html: `<span class='font-weight-bolder'>${searchResult.toUpperCase()}</span>`
-//        });
-//    }
-//    else {
-//        //create new Select option
-//        let newOption = new Option(tagEntry.value, tagEntry.value);
-//        document.getElementById("TagList").options[index++] = newOption;
-//    }
-
-//    //clear the entry box
-//    tagEntry.value = "";
-//    return true;
-//}
-
-//function DeleteTag() {
-//    let tagCount = 1;
-//    let tagList = document.getElementById("TagList");
-//    if (!tagList) return false;
-
-//    if (tagList.selectedIndex == -1) {
-//        swalWithDarkButton.fire({
-//            html: `<span class='font-weight-bolder'>NO TAGS SELECTED TO DELETE</span>`
-//        });
-//        return true;
-//    }
-
-//    while (tagCount > 0) {
-//        if (tagList.selectedIndex >= 0) {
-//            tagList.options[tagList.selectedIndex] = null;
-//            --tagCount;
-//        }
-//        else
-//            tagCount = 0;
-//        index--;
-//    }
-//}
-//$("form").on("submit", function () {
-//    $("#TagList option").prop("selected", "selected");
-//})
-
-//if (typeof tagValues !== 'undefined' && tagValues != '') {
-//    let tagArray = tagValues.split(",");
-//    for (let loop = 0; loop < tagArray.length; loop++) {
-//        ReplaceTag(tagArray[loop], loop);
-//        index++;
-//    }
-//}
-
-//function ReplaceTag(tag, index) {
-//    let newOption = new Option(tag, tag);
-//    document.getElementById("TagList").options[index] = newOption;
-//}
-
-////search function for duplicate tags on the post & return error if found
-//function Search(str) {
-//    if (str == "") {
-//        return `EMPTY tags are not allowed.`;
-//    }
-
-//    var tagsEl = document.getElementById('TagList');
-//    if (tagsEl) {
-//        let tags = tagsEl.options;
-//        //for (let i = 0; i < options.length; i++) {
-//        //    if (options[i].value == str) {
-//        //        return '#${str} was found to be a DUPLICATE tag for this post. Please enter a new tag.';
-//        //    }
-//        //}
-//        for (let i = 0; i < tags.length; i++) {
-//            if (tags[i].value == str) {
-//                return `#${str} was found to be a DUPLICATE tag for this post. Please enter a new tag.`;
-//            }
-//        }
-//    }
-//}
-//
-//sweet alert
-//const swalWithDarkButton = Swal.mixin({
-//    customClass: {
-//        confirmButton: `btn btn-danger btn-sm btn-block btn-outline-dark`
-//    },
-//    imageUrl: '/img/doh.jpg',
-//    imageHeight: 200,
-//    imageAlt: 'Homer Simpson: saying, "Doh!"',
-//    timer: 3000,
-//    buttonsStyling: false
-//});
-
-//To be fair, I did do the hard work above. As my main focus is C#, rather than JavaScript, I was curious if ChatGPT could optimize my JS code and make it more "Enterprise" level.
-//I ran a few tests and it seems to check out.
-//Below is a more streamlined version that does the same things as the above code.
-//-----------------------------------------------------------------------------------------------//
-// Enterprise-Grade JavaScript Code (According to ChatGPT):
-
-// Constants
+﻿// Constants
+//the selected property
 const SELECTED = "selected";
+
+// Custom alert
+const swalWithDarkButton = Swal.mixin({
+    customClass: {
+        confirmButton: `btn btn-danger btn-sm btn-block btn-outline-dark`
+    },
+    imageUrl: '/img/doh.jpg',
+    imageHeight: 200,
+    imageAlt: 'Homer Simpson: saying, "Doh!"',
+    timer: 3000,
+    buttonsStyling: false
+});
 
 // Variables
 let index = 0;
 
 // Functions
+
+// Add tag function
 function AddTag() {
     // Use search to detect duplicate tags
     let tagEntry = document.getElementById("TagEntry");
+    if (!tagEntry.value) {
+        showError("EMPTY tags are not allowed.");
+        return false;
+    }
     let searchResult = Search(tagEntry.value);
 
     if (searchResult !== null) {
         showError(`#${searchResult} was found to be a DUPLICATE tag for this post. Please enter a new tag.`);
+        return false;
     } else {
         // Create a new Select option
         let newOption = new Option(tagEntry.value, tagEntry.value);
@@ -126,6 +43,7 @@ function AddTag() {
     return true;
 }
 
+// Delete tag function
 function DeleteTag() {
     let tagList = document.getElementById("TagList");
     if (!tagList) return false;
@@ -141,16 +59,14 @@ function DeleteTag() {
     }
 }
 
+//replace tag function
 function ReplaceTag(tag, position) {
     let newOption = new Option(tag, tag);
     document.getElementById("TagList").options[position] = newOption;
 }
 
+//search function
 function Search(str) {
-    if (str === "") {
-        return "EMPTY tags are not allowed.";
-    }
-
     let tagsEl = document.getElementById("TagList");
     if (tagsEl) {
         let tags = tagsEl.options;
@@ -163,8 +79,10 @@ function Search(str) {
     return null;
 }
 
+//show alert
 function showError(message) {
-    Swal.fire({
+    //Swal.fire({
+    swalWithDarkButton.fire({
         title: "Error",
         text: message,
         icon: "error",
