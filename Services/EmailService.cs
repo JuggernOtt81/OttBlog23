73b6fd52192e6e0e7184df1c93d9036a483b5f19
JuggernOtt81 <juggernott81@gmail.com>
2023-02-05 15:32:13 -0500

    email cleanup for railway

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -26,8 +26,8 @@ namespace OttBlog23.Services
         public async Task SendContactEmailAsync(string email, string name, string subject, string message)
         {
             var newEmail = new MimeMessage();
-            newEmail.Sender = MailboxAddress.Parse(_mailSettings.MailEmail);
-            newEmail.To.Add(MailboxAddress.Parse(_mailSettings.MailEmail));
+            newEmail.Sender = MailboxAddress.Parse(_mailSettings.Email);
+            newEmail.To.Add(MailboxAddress.Parse(_mailSettings.Email));
             newEmail.Subject = subject;
 
             var builder = new BodyBuilder();
@@ -37,7 +37,7 @@ namespace OttBlog23.Services
 
             using var smtp = new SmtpClient();
             smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.StartTls);
-            smtp.Authenticate(_mailSettings.MailEmail, _mailSettings.MailPassword);
+            smtp.Authenticate(_mailSettings.Email, _mailSettings.MailPassword);
 
             await smtp.SendAsync(newEmail);
 
@@ -47,7 +47,7 @@ namespace OttBlog23.Services
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             var newEmail = new MimeMessage();
-            newEmail.Sender = MailboxAddress.Parse(_mailSettings.MailEmail);
+            newEmail.Sender = MailboxAddress.Parse(_mailSettings.Email);
             newEmail.To.Add(MailboxAddress.Parse(email));
             newEmail.Subject = subject;
 
@@ -60,7 +60,7 @@ namespace OttBlog23.Services
 
             using var smtp = new SmtpClient();
             smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.StartTls);
-            smtp.Authenticate(_mailSettings.MailEmail, _mailSettings.MailPassword);
+            smtp.Authenticate(_mailSettings.Email, _mailSettings.MailPassword);
 
             await smtp.SendAsync(newEmail);
             smtp.Disconnect(true);
--- ViewModels/MailSettings.cs
+++ ViewModels/MailSettings.cs
@@ -3,8 +3,8 @@
     public class MailSettings
     {
         //to configure and use an smtp server (google)
-        public string? MailEmail { get; set; }
-        public string? MailDisplayName { get; set; }
+        public string? Email { get; set; }
+        public string? DisplayName { get; set; }
         public string? MailPassword { get; set; }
         public string? MailHost { get; set; }
         public int MailPort { get; set; }
