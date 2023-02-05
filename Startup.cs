8ad61680f925ec4c3fcf7e54458aadc7ad1eac2d
JuggernOtt81 <juggernott81@gmail.com>
2023-02-05 14:23:00 -0500

    env var 2

--- Startup.cs
+++ Startup.cs
@@ -15,7 +15,6 @@ namespace OttBlog23
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
             Configuration = builder.Build();
         }
